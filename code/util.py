1305a6a19d60b222907f246816b1d4eb489a09e4
noggl <67533630+noggl@users.noreply.github.com>
2023-06-13 17:18:21 -0400

    Remove year in parantheses from name when it comes from TVDB

--- code/util.py
+++ code/util.py
@@ -251,6 +251,9 @@ def addMapping(item):
 def animeMatch(result, show):
     matching = 0
     # Check romanji and english title VS all titles in results
+    #if result['title'] contains year in paranthesis, remove the year
+    if re.search(r'\(\d{4}\)', result['title']):
+        result['title'] = re.sub(r'\(\d{4}\)', '', result['title']).rstrip()
     for title in show['titles'].values():
         if title is None:
             continue
